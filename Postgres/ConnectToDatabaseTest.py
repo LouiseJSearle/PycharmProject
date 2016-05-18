52d72e5cde69b9700f8adfa5d10e2e6384e0bb04
Louise Searle <louise.searle@wur.nl>
2016-05-18 14:29:44 +0200

    Testing pg8000 Reading a table okay

--- Postgres/ConnectToDatabaseTest.py
+++ Postgres/ConnectToDatabaseTest.py
@@ -3,6 +3,12 @@ import pg8000
 # Connect to database
 
 conn = pg8000.connect(user='postgres', password='postgres', database='postgres')
+
+# Create cursor
+
 cursor = conn.cursor()
+
+# Select rows in table
+
 cursor.execute("SELECT * FROM people_and_photos_per_country")
 results = cursor.fetchall()
