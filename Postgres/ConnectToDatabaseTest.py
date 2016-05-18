91fb6e3f6ba52df46f938f60f3211ad080bacf7d
Louise Searle <louise.searle@wur.nl>
2016-05-18 17:42:53 +0200

    Playing with how to execute SQL files.

--- Postgres/ConnectToDatabaseTest.py
+++ Postgres/ConnectToDatabaseTest.py
@@ -3,15 +3,4 @@ import pg8000
 # Connect to database
 
 conn = pg8000.connect(user='postgres', password='postgres', database='thesisdb')
-
-# Create cursor
-
 cursor = conn.cursor()
-
-# Select rows in table
-
-cursor.execute("SELECT * FROM point_clusters_50_250")
-results = cursor.fetchall()
-assert isinstance(results, object)
-for row in results:
-    print row
---
+++ Postgres/SQLtests.py
@@ -0,0 +1,20 @@
+
+
+# FUNCTION:
+# Open table, make list of pks.
+#
+# Read clusters from table into a list.
+#
+# For each cluster:
+#
+# clusterName =
+#
+# FUNCTION: Execute SQL script.
+# Open SQL file
+# Read file
+# Close file
+#
+
+# query = "SELECT * FROM photo_tourist_ams WHERE ST_Contains AS " + clusterName
+# cursor.execute(query)
+# results = cursor.fetchall()
